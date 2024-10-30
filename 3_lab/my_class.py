691328c9d54417ac6a9c65883d964a1410dbc6bb
bbuhyl <bbuhyl@cisco.com>
2024-10-30 09:04:02 +0200

    пройшли методи

--- 3_lab/my_class.py
+++ 3_lab/my_class.py
@@ -18,10 +18,11 @@ class MySuperClass:
         self.mark = mark # public публічний атрибук
         self.group = group
         self._age = None # (protected) захищений атрибут
+        self._scholarship = 0
     
     @property
     def name(self):
-        """Ця властивість є затритою, її можна читати але не можна змінювати
+        """Ця властивість є закритою, її можна читати але не можна змінювати
         """
         return self.__name
     
@@ -29,11 +30,45 @@ class MySuperClass:
     def surname(self):
         return self.__surname
     
+    @property
+    def say_hello(self):
+        a = 1 + 2
+        return f"Привіт {a}"
+    
     def __repr__(self):
         return "Представлення обєкту Студент, його задають: MySuperClass(surname, name, mark)"
     
     def __len__(self):
         return len(self.surname)
+    
+    def fucntion_in_class(self):
+        """Це вже метод згідно термінології, і він публічний
+        """
+        return "Ми викликали публічний метод"
+
+    def _protected_method_in_class(self):
+        """Це захищений метод
+        """
+        self.__this_is_private()
+        return "Ми доступаємось до захищеного методу"
+    
+    def __this_is_private(self):
+        print("Це приватний метод!")
+
+    def calculate_scholarship_after_session(self, raiting: int):
+        if raiting == 5:
+            self._scholarship = "1800 грн"
+            return "Присвоєно підвищену стипундію"
+        if raiting == 4:
+            self._scholarship = "1400 грн"
+            return "Присвоєно звичайну стипундію"
+        self._scholarship = 0
+        return "Рейтинг занизький для стипендії"
+    
+    def panishment(self):
+        return "Ми прийшли додому і мама нас насварила за погані оцінки"
 
 def function_in_module():
+    """Це просто функція (згідно загальної термінології)
+    """
     pass
